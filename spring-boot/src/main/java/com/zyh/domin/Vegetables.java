523eb014bd9343196021a88d28fd319540add69e
potianz <1648919702@qq.com>
2021-08-03 19:53:30 +0800

    使用了从properties文件中读取信息。

--- spring-boot/src/main/java/com/zyh/domin/Vegetables.java
+++ spring-boot/src/main/java/com/zyh/domin/Vegetables.java
@@ -1,34 +1,40 @@
 package com.zyh.domin;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Data
 public class Vegetables {
     private String potato;
     private String eggplant;
     private String greenpeper;
 
-    public Vegetables() {
-    }
-
-    public String getPotato() {
-        return potato;
-    }
-
-    public void setPotato(String potato) {
-        this.potato = potato;
-    }
-
-    public String getEggplant() {
-        return eggplant;
-    }
-
-    public void setEggplant(String eggplant) {
-        this.eggplant = eggplant;
-    }
-
-    public String getGreenpeper() {
-        return greenpeper;
-    }
-
-    public void setGreenpeper(String greenpeper) {
-        this.greenpeper = greenpeper;
-    }
+//    public Vegetables() {
+//    }
+//
+//    public String getPotato() {
+//        return potato;
+//    }
+//
+//    public void setPotato(String potato) {
+//        this.potato = potato;
+//    }
+//
+//    public String getEggplant() {
+//        return eggplant;
+//    }
+//
+//    public void setEggplant(String eggplant) {
+//        this.eggplant = eggplant;
+//    }
+//
+//    public String getGreenpeper() {
+//        return greenpeper;
+//    }
+//
+//    public void setGreenpeper(String greenpeper) {
+//        this.greenpeper = greenpeper;
+//    }
 }
