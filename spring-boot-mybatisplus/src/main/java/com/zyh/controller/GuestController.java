401ff259a6d42e38c273d4a3ee2220bf5ed5a047
potianz <1648919702@qq.com>
2021-08-08 21:10:26 +0800

    事务处理注解@Transactional的使用方法，和位置。

--- spring-boot-mybatisplus/src/main/java/com/zyh/controller/GuestController.java
+++ spring-boot-mybatisplus/src/main/java/com/zyh/controller/GuestController.java
@@ -4,10 +4,13 @@ import com.zyh.model.Guest;
 import com.zyh.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.beans.Transient;
 import java.util.List;
 
 @Controller
@@ -21,7 +24,18 @@ public class GuestController {
     public String  selectAll(Model model){
         List<Guest> list = guestService.list();
         model.addAttribute("guestList",list);
-        System.out.println("我执行了");
         return "guestindex";
+    }
+    @GetMapping("/toAdd")
+    public String toAdd(){
+        return "/guestAdd";
+    }
+    @Transactional
+    @PostMapping
+    public String add(Guest guest){
+        guest.setId(5555L);
+//        int result = 1/0;
+        guestService.save(guest);
+        return "redirect:/guest";
     }
 }
